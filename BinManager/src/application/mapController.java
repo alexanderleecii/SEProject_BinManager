f31801fbe508c8fdc9225796de20b39d80a41cc6
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2019-12-20 11:17:39 +0100

    Debut menu - ATTENTION pas fini entierement, qques modifs a faire encore

--- BinManager/src/application/mapController.java
+++ BinManager/src/application/mapController.java
@@ -1,30 +1,27 @@
 package application;
 
+//import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
+import BinManagerFacade.PersonFacade;
+//import BinManagerPerson.Person;
+import javafx.event.*;
 import javafx.fxml.FXML;
+//import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class homeCitizenController implements Initializable {
-	
+public class mapController implements Initializable {
 		@FXML
 		private MenuBar menu;
-		
+	
 	   @Override
 	   public void initialize(URL location, ResourceBundle resources) {
-	 	      
+	      
 	   }
 	   
 	   public void mapAction(ActionEvent event) {
-		   System.out.println("map");
-		   Routing rout= new Routing();
-		   rout.goTo("mapPage");
-	   }
-	   
-	   public void mapAction2(ActionEvent event) {
 		   System.out.println("map");
 		   Routing rout= new Routing();
 		   rout.goTo("mapPage");
